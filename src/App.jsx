82b4055e36c11f410bc55acd9df2a21ff390bb53
Letho Norgil <theo.grillon@laposte.net>
2023-01-20 15:58:44 +0100

    Functionning code

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,39 @@
-import './App.css';
+import './app.css';
 import {useState} from "react";
+import { useEffect } from 'react';
 
 function App() {
 
-    const [newItem, setNewItem] = useState("");
-    const[tasks, setTasks] = useState([]);
 
-    function addItem(){
+    const [city, setCity] = useState("");
+    const [weather, setWeather] = useState("");
+    const [heat, setHeat] = useState("");
 
-        if(!newItem){
-            alert("Enter an item");
-            return;
-        }
-        const item = {
-            id: Math.floor(Math.random()*1000),
-            value: newItem
-        }
+    useEffect(()=>{
+        fetch("https://izudztw6jk.execute-api.eu-west-1.amazonaws.com/Prod/geo?lon=12.aze&lat=31.e")
+            .then((el) => el.json())
+            .then((res) => setCity(res.city.toUpperCase()))
+    }, []);
+    console.log(city);
+    useEffect(() =>{
+        fetch("https://izudztw6jk.execute-api.eu-west-1.amazonaws.com/Prod/weather/"+city)
+            .then((el) => el.json())
+            .then((res) => setWeather(res.condition))
+    }, [city]);
 
-        setTasks(oldList => [...oldList, item]);
-        setNewItem("");
-    }
-
-    function deleteItem(id){
-        const newArray = tasks.filter(item => item.id !== id);
-        setTasks(newArray);
-    }
+    useEffect(()=>{
+        fetch("https://izudztw6jk.execute-api.eu-west-1.amazonaws.com/Prod/weather/"+city)
+            .then((el) =>el.json())
+            .then((res) => setHeat(res.temperature))
+    }, []);
 
     return (
-        <div className="App">
-            <h1>My ToDo List</h1>
+        <div className="weather_api">
+            
+            <h1>{city} - FRANCE</h1>
 
-            <p>You have {tasks.length} tasks left to do !</p>
-
-            <input
-                onKeyPress={(e) => {if (e.key === "Enter") {
-                    // Cancel the default action, if needed
-                    e.preventDefault();
-                    // Trigger the button element with a click
-                    document.getElementById("addButton").click();
-                }}}
-                id="myInput"
-                type="text"
-                placeholder="Add a task"
-                value={newItem}
-                onChange={e => setNewItem(e.target.value)}
-            />
-
-            <button
-                id="addButton"
-                type="submit"
-                onClick={() => addItem()}
-            >Add
-            </button>
-
-            <ul>
-                {
-                    tasks.map(item => {
-                    return(
-                        <li key={item.id}>{item.value}
-                            <input type="checkbox"/>
-                            <button type="button" className="delete" onClick={() => deleteItem(item.id)}>🗑️</button>
-                        </li>
-                    )
-                    })
-                }
-            </ul>
+            <p>{heat}°C</p>
+            <p>{weather}</p>
         </div>
     );
 }
